17e929aa5eec580eb609028c250de73a97576f55
Jefferson K. Schmitt <jeffersonschmitt@live.com>
2016-01-19 18:00:01 -0200

    Update 19/01 v1.02

--- app/src/main/java/jeffersonschmitt/github/com/helloword/MainActivity.java
+++ app/src/main/java/jeffersonschmitt/github/com/helloword/MainActivity.java
@@ -3,39 +3,31 @@ package jeffersonschmitt.github.com.helloword;
 import android.app.Activity;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.support.annotation.UiThread;
+import jeffersonschmitt.github.com.helloword.business.UserServices;
+import jeffersonschmitt.github.com.helloword.contracts.IUserService;
+import jeffersonschmitt.github.com.helloword.domain.User;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
-import org.w3c.dom.Text;
-import android.support.v7.app.AppCompatActivity;
-
-import jeffersonschmitt.github.com.helloword.business.UserServices;
-import jeffersonschmitt.github.com.helloword.contracts.IUserService;
-import jeffersonschmitt.github.com.helloword.domain.User;
-
 
 @EActivity(R.layout.activity_main)
 
 public class MainActivity extends Activity {
 
-    @ViewById
-    EditText usuario;
-    @ViewById
-    EditText senha;
-    @ViewById Button entrar;
+  @ViewById EditText usuario;
+  @ViewById EditText senha;
+  @ViewById Button entrar;
 
-    IUserService userService;
+  IUserService userService;
 
+  @AfterViews void init() {
+    userService = new UserServices();
+  }
 
-    @AfterViews void init(){
-      userService = new UserServices();
-   }
-    @Click void add(){
-        String usuarioUser= usuario.getText().toString();
-        String senhaUser= senha.getText().toString();
-        User user= new User(usuario,senha); // problema aqui.
-    }
+  @Click void add() {
+    String usuarioUser = usuario.getText().toString();
+    String senhaUser = senha.getText().toString();
+    User user = new User(usuarioUser, senhaUser);
+  }
 }
--- app/src/main/java/jeffersonschmitt/github/com/helloword/domain/User.java
+++ app/src/main/java/jeffersonschmitt/github/com/helloword/domain/User.java
@@ -2,29 +2,38 @@ package jeffersonschmitt.github.com.helloword.domain;
 
 import com.activeandroid.Model;
 
-public class User extends Model{
-    private int usuario;
-    private String senha;
-    private double saldo;
-    private String cpf;
+public class User extends Model {
+  private String usuario;
+  private String senha;
+  private double saldo;
+  private String cpf;
 
-    public int getUsuario() {
-        return usuario;
-    }
+  public User() {
+    super();
+  }
 
-    public String getSenha() {
-        return senha;
-    }
+  public User(String usuario, String senha) {
+    this.usuario = usuario;
+    this.senha = senha;
+  }
 
-    public double getSaldo() {
-        return saldo;
-    }
+  public String getUsuario() {
+    return usuario;
+  }
 
-    public String getCpf() {
-        return cpf;
-    }
+  public String getSenha() {
+    return senha;
+  }
 
-    public void changeSenha(String senha){
-        this.senha=senha;
-    }
+  public double getSaldo() {
+    return saldo;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void changeSenha(String senha) {
+    this.senha = senha;
+  }
 }
